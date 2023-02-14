633bc338e37ea4eabb86582cd315ed1330f08447
Micheal <mlowell94@gmail.com>
2023-02-14 14:35:09 -0500

    Add new project

--- src/projectData.js
+++ src/projectData.js
@@ -1,57 +1,66 @@
 const projectData = {
     p1: {
+        name: 'Engage Estimator',
+        url: 'engage.gif',
+        git: 'https://github.com/mlowell94/engage-estimator',
+        live: 'https://mlowell94.github.io/engage-estimator/',
+        desc: "An app for estimating character stats in Fire Emblem: Engage.",
+        technologyIcons: ['css-3.svg', 'html-5.svg', 'javascript.svg', 'react.svg'],
+        animationID: 1
+    },
+    p2: {
         name: 'Memory Card',
         url: 'memory-card.gif',
         git: 'https://github.com/mlowell94/memory-card',
         live: 'https://mlowell94.github.io/memory-card/',
         desc: "A memory game in which the player clicks an image they haven't clicked on before.",
         technologyIcons: ['css-3.svg', 'html-5.svg', 'javascript.svg', 'react.svg', 'react-spring.svg'],
-        animationID: 1
+        animationID: 2
     },
-    p2: {
+    p3: {
         name: 'Recipe Book',
         url: 'recipe-book.gif',
         git: 'https://github.com/mlowell94/cookbook-public',
         live: '',
         desc: "A fullstack project built in under a week. The user can create, update, and organize recipes in several different ways.",
         technologyIcons: ['css-3.svg', 'html-5.svg', 'javascript.svg', 'react.svg', 'nodejs-icon.svg', 'react-router.svg', 'express.svg'],
-        animationID: 2
+        animationID: 3
     },
-    p3: {
+    p4: {
         name: 'Weather App',
         url: 'weather-app.gif',
         git: 'https://github.com/mlowell94/weather-app',
         live: 'https://mlowell94.github.io/weather-app/',
         desc: "A Windows 2000-themed weather app that provides the weather, and approximate local time for a given location.",
         technologyIcons: ['css-3.svg', 'html-5.svg', 'javascript.svg', 'react.svg'],
-        animationID: 3
+        animationID: 4
     },
-    p4: {
+    p5: {
         name: 'Shopping Cart',
         url: 'shopping-cart.gif',
         git: 'https://github.com/mlowell94/shopping-cart',
         live: 'https://mlowell94.github.io/shopping-cart/',
         desc: "A Resident Evil 4 themed shopping site mockup built as an exercise in testing react functions and components.",
         technologyIcons: ['css-3.svg', 'html-5.svg', 'javascript.svg', 'react.svg', 'react-router.svg', 'framer.svg'],
-        animationID: 4
+        animationID: 5
     },
-    p5: {
+    p6: {
         name: 'Portfolio',
         url: 'portfolio.gif',
         git: 'https://github.com/mlowell94/portfolio',
         live: 'https://mlowell94.github.io/portfolio/',
         desc: "This website! Built to host my projects and provide basic information about myself.",
         technologyIcons: ['css-3.svg', 'html-5.svg', 'javascript.svg', 'react.svg', 'react-router.svg', 'framer.svg'],
-        animationID: 5
+        animationID: 6
     },
-    p6: {
+    p7: {
         name: 'Battleship',
         url: 'battleship.gif',
         git: 'https://github.com/mlowell94/battleship',
         live: 'https://mlowell94.github.io/battleship/',
         desc: "A version of the game Battleship that can be played in the web browser. Built from scratch with Javascript, and tested with Jest.",
         technologyIcons: ['css-3.svg', 'html-5.svg', 'javascript.svg', 'webpack.svg'],
-        animationID: 6
+        animationID: 7
     },
 }
 
